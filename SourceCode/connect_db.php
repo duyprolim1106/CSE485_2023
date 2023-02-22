6ccbed170b180ce06de354803f851635e0cefe3a
duybn1106 <duynguyen11062002@gmail.com>
2023-02-22 16:27:11 +0700

    commit

---
+++ SourceCode/connect_db.php
@@ -0,0 +1,8 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', '', 'BTTH01_CSE485');
+    if (!$con) {
+        die('Kết nối tới Sever thất bại.');
+    } else {
+        echo "connect";
+    }
+?>
