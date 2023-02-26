6e79c762603a95126facfc4ecf72f09a18fe6db4
duybn1106 <duynguyen11062002@gmail.com>
2023-02-26 08:58:43 +0700

    Duy xong phần thêm, sửa, xoá thể loại

--- SourceCode/admin/add_author.php
+++ SourceCode/admin/add_author.php
@@ -3,12 +3,6 @@
     include '../connect_db.php';
 ?>
     <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
-       <!-- $sql = "INSERT INTO tacgia 
-        value()
-       
-       " -->
-       
         <div class="row">
             <div class="col-sm">
                 <h3 class="text-center text-uppercase fw-bold">Thêm mới tác giả </h3>
@@ -18,7 +12,6 @@
                         <span class="input-group-text" id="lblCatName">Tên tác giả</span>
                         <input type="text" class="form-control" name="txtCatName" >
                     </div>
-
                     <div class="form-group  float-end ">
                         <input type="submit" value="Thêm" class="btn btn-success">
                         <a href="category.php" class="btn btn-warning ">Quay lại</a>
