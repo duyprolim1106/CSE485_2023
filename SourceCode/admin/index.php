657ac17d2db46c033ea7457c7c8dfa2f61c7677c
kimbau910 <99164949+kimbau910@users.noreply.github.com>
2023-02-28 11:44:33 +0700

    code thống kê người dùng

--- SourceCode/admin/index.php
+++ SourceCode/admin/index.php
@@ -1,66 +1,79 @@
 <?php
     include 'components/header_admin.php';
 ?>
-    <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Người dùng</a>
-                        </h5>
+<?php 
+include '../connect_db.php';
 
-                        <h5 class="h1 text-center">
-                            110
-                        </h5>
-                    </div>
-                </div>
-            </div>
+    function getCount(string $table)
+        {
+            global $con;
+            $sql = "SELECT COUNT(*) AS so_luong FROM " . $table;
+            $result = mysqli_query($con, $sql);
+            $row = mysqli_fetch_assoc($result);
+            return $row['so_luong'];
+        }
 
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Thể loại</a>
-                        </h5>
+?>
+<main class="container mt-5 mb-5">
+    <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Người dùng</a>
+                    </h5>
 
-                        <h5 class="h1 text-center">
-                            10
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Tác giả</a>
-                        </h5>
-
-                        <h5 class="h1 text-center">
-                            20
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Bài viết</a>
-                        </h5>
-
-                        <h5 class="h1 text-center">
-                            110
-                        </h5>
-                    </div>
+                    <h5 class="h1 text-center">
+                        <?= getCount('users')?>
+                    </h5>
                 </div>
             </div>
         </div>
-    </main>
+
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Thể loại</a>
+                    </h5>
+
+                    <h5 class="h1 text-center">
+                        <?= getCount('theloai')?>
+                    </h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Tác giả</a>
+                    </h5>
+
+                    <h5 class="h1 text-center">
+                        <?= getCount('tacgia')?>
+                    </h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Bài viết</a>
+                    </h5>
+
+                    <h5 class="h1 text-center">
+                        <?= getCount('baiviet')?>
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php
     include 'components/footer_admin.php';
--- SourceCode/index.php
+++ SourceCode/index.php
@@ -1,12 +1,14 @@
 <?php
 include 'header.php';
 ?>
-
 <div id="carouselExampleIndicators" class="carousel slide">
     <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
@@ -19,11 +21,13 @@ include 'header.php';
             <img src="images/slideshow/slide03.jpg" class="d-block w-100" alt="...">
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
@@ -37,17 +41,18 @@ include 'header.php';
         $result = mysqli_query($con, $sql);
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-                <div class="col-sm-3">
-                    <div class="card mb-2" style="width: 100%; height: 250px;">
-                        <img src="<?php echo $row['hinhanh'] ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">
-                                <a href="./detail.php?tieude=<?php echo $row['tieude']; ?>" class="text-decoration-none"><?php echo $row['tieude']; ?></a>
-                            </h5>
-                        </div>
-                    </div>
+                ?>
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%; height: 250px;">
+                <img src="<?php echo $row['hinhanh'] ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="./detail.php?tieude=<?php echo $row['tieude']; ?>" class="text-decoration-none"><?php
+                                   echo $row['tieude']; ?></a>
+                    </h5>
                 </div>
+            </div>
+        </div>
         <?php
             }
         }
