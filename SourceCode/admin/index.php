1645a14527e6825757ac0c1ab99430e2b8716991
kimbau910 <99164949+kimbau910@users.noreply.github.com>
2023-02-28 11:51:18 +0700

    code phần thống kê

--- SourceCode/admin/index.php
+++ SourceCode/admin/index.php
@@ -3,7 +3,6 @@
 ?>
 <?php 
 include '../connect_db.php';
-
     function getCount(string $table)
         {
             global $con;
@@ -15,7 +14,6 @@ include '../connect_db.php';
 
 ?>
 <main class="container mt-5 mb-5">
-    <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
     <div class="row">
         <div class="col-sm-3">
             <div class="card mb-2" style="width: 100%;">
